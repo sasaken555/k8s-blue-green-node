d20f9d8b81cb07ff52a2bdd0b7659649b7253d29
Kentaro <accelerk@gmail.com>
2018-07-11 23:50:27 +0900

    ECRからのデプロイ失敗時指摘事項を修正

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'V1-App' });
+  res.render('index', { title: 'V2-App' });
 });
 
 module.exports = router;
